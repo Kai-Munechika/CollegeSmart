cf179db4596d18603e733d743a3d24f01f5da2a0
Kai Munechika <kaim808@gmail.com>
2016-10-13 15:32:40 -0400

    Minor tweaks/fixes
    changed EditText fields to take only numbers/decimal and fixed a layout
    bug

--- app/src/main/java/edu/kaimbu/collegeapp0/MainActivity.java
+++ app/src/main/java/edu/kaimbu/collegeapp0/MainActivity.java
@@ -1,20 +1,33 @@
 package edu.kaimbu.collegeapp0;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText tuitionAmount = (EditText) findViewById(R.id.tuitionAmount);
+        final EditText finAidAmount = (EditText) findViewById(R.id.finAidAmount);
+        final EditText loansAmount = (EditText) findViewById(R.id.loansAmount);
+        final EditText pocketCostAmount = (EditText) findViewById(R.id.pocketCostAmount);
+        final EditText expectedSalaryAmount = (EditText) findViewById(R.id.expectedSalaryAmount);
+
     }
 
     public void calculatePressed(View view) {
         Intent calculate = new Intent(this, secondPage.class);
         startActivity(calculate);
     }
+
+
+
 }
