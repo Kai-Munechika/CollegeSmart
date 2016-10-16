3ba285ca358e2f984d8bbedeba2195c65f578861
Kai Munechika <kaim808@gmail.com>
2016-10-16 13:56:13 -0400

    Implemented functionality and connection between tabs
    Crude implementation; should reimplement later with fragments/separate
    java classes and xml files

--- app/src/main/java/edu/kaimbu/collegeapp0/secondPage.java
+++ app/src/main/java/edu/kaimbu/collegeapp0/secondPage.java
@@ -13,6 +13,8 @@ import android.widget.TextView;
 import java.util.HashMap;
 import java.util.Map;
 
+import static edu.kaimbu.collegeapp0.R.id.tab2;
+
 public class secondPage extends AppCompatActivity {
     TabHost tabHost;
 
@@ -23,26 +25,28 @@ public class secondPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_page);
 
-        TabHost host = (TabHost)findViewById(R.id.tabHost);
+        final TabHost host = (TabHost)findViewById(R.id.tabHost);
         host.setup();
 
         //Tab 1
-        TabHost.TabSpec spec = host.newTabSpec("Five Years");
-        spec.setContent(R.id.tab1);
-        spec.setIndicator("Five Years");
-        host.addTab(spec);
+        final TabHost.TabSpec spec1 = host.newTabSpec("Five Years");
+        spec1.setContent(R.id.tab1);
+        spec1.setIndicator("Five Years");
+        host.addTab(spec1);
 
         //Tab 2
-        spec = host.newTabSpec("Ten Years");
-        spec.setContent(R.id.tab2);
-        spec.setIndicator("Ten Years");
-        host.addTab(spec);
+        final TabHost.TabSpec spec2 = host.newTabSpec("Ten Years");
+        spec2.setContent(tab2);
+        spec2.setIndicator("Ten Years");
+        host.addTab(spec2);
 
         //Tab 3
-        spec = host.newTabSpec("Fifteen Years");
-        spec.setContent(R.id.tab3);
-        spec.setIndicator("Fifteen Years");
-        host.addTab(spec);
+        final TabHost.TabSpec spec3 = host.newTabSpec("Fifteen Years");
+        spec3.setContent(R.id.tab3);
+        spec3.setIndicator("Fifteen Years");
+        host.addTab(spec3);
+
+
 
         // naive implementation; will figure out a better way to do this.
         // rent, transport, food, total respectively
@@ -67,6 +71,13 @@ public class secondPage extends AppCompatActivity {
         final TextView monthlyLoanPayment   = (TextView) findViewById(R.id.monthlyLoanPayment);
         final TextView monthlyBalance1      = (TextView) findViewById(R.id.monthlyBalance1);
 
+        final TextView monthlyincomePostTax_2 = (TextView) findViewById(R.id.incomePostTax_2);
+        final TextView monthlyLoanPayment_2   = (TextView) findViewById(R.id.monthlyLoanPayment_2);
+        final TextView monthlyBalance1_2      = (TextView) findViewById(R.id.monthlyBalance1_2);
+
+        final TextView monthlyincomePostTax_3 = (TextView) findViewById(R.id.incomePostTax_3);
+        final TextView monthlyLoanPayment_3   = (TextView) findViewById(R.id.monthlyLoanPayment_3);
+        final TextView monthlyBalance1_3      = (TextView) findViewById(R.id.monthlyBalance1_3);
         // pull data from previous Activity
         Intent intent = getIntent();
 
@@ -76,7 +87,9 @@ public class secondPage extends AppCompatActivity {
         // for monthly income post tax
         // avg monthly income post tax = expectedSalary * (1 - 0.01 * income tax) / 12
         /* gotta change this later, tax != 28% for every state/tax bracket */
-        monthlyincomePostTax.setText( String.valueOf( (int) (userEntries[4]*0.72/12) ));
+        monthlyincomePostTax  .setText( String.valueOf( (int) (userEntries[4]*0.72/12) ));
+        monthlyincomePostTax_2.setText( String.valueOf( (int) (userEntries[4]*0.72/12) ));
+        monthlyincomePostTax_3.setText( String.valueOf( (int) (userEntries[4]*0.72/12) ));
 
         // for average monthly loan payment
         double rate = 0.07;
@@ -84,7 +97,19 @@ public class secondPage extends AppCompatActivity {
         int years   =    5; 		// 10 and 15 for the other tabs
                                       // * 4 to calculate total loans over 4 years
         double loanMath = userEntries[2] * 4 * (rate/n) / ( 1 -  Math.pow( ( 1 +  (rate/n) ) , (-n*years)  ) );
+
         monthlyLoanPayment.setText( String.valueOf( (int) loanMath));
+
+        years = 10;
+        loanMath = userEntries[2] * 4 * (rate/n) / ( 1 -  Math.pow( ( 1 +  (rate/n) ) , (-n*years)  ) );
+
+        monthlyLoanPayment_2.setText( String.valueOf( (int) loanMath));
+
+        years = 15;
+        loanMath = userEntries[2] * 4 * (rate/n) / ( 1 -  Math.pow( ( 1 +  (rate/n) ) , (-n*years)  ) );
+        monthlyLoanPayment_3.setText( String.valueOf( (int) loanMath));
+
+
 
 
         // for monthly balance
@@ -94,6 +119,15 @@ public class secondPage extends AppCompatActivity {
 
         monthlyBalance1.setText(String.valueOf(temp1 - temp2));
 
+        int temp1_2 = Integer.parseInt(monthlyincomePostTax_2.getText().toString());
+        int temp2_2 = Integer.parseInt(monthlyLoanPayment_2.getText()  .toString());
+
+        monthlyBalance1_2.setText(String.valueOf(temp1_2 - temp2_2));
+
+        int temp1_3 = Integer.parseInt(monthlyincomePostTax_3.getText().toString());
+        int temp2_3 = Integer.parseInt(monthlyLoanPayment_3.getText()  .toString());
+
+        monthlyBalance1_3.setText(String.valueOf(temp1_3 - temp2_3));
 
         /*
 
@@ -112,9 +146,33 @@ public class secondPage extends AppCompatActivity {
         final TextView difference   = (TextView) findViewById(R.id.difference);
         final Spinner s = (Spinner) findViewById(R.id.spinner);
 
+
+        final TextView rentVal_2      = (TextView) findViewById(R.id.rentVal_2);
+        final TextView transportVal_2 = (TextView) findViewById(R.id.transportVal_2);
+        final TextView foodVal_2      = (TextView) findViewById(R.id.foodVal_2);
+
+        final TextView total_2        = (TextView) findViewById(R.id.total_2);
+        final TextView balance2_2     = (TextView) findViewById(R.id.monthlyBalance2_2);
+        final TextView difference_2   = (TextView) findViewById(R.id.difference_2);
+        final Spinner s_2 = (Spinner) findViewById(R.id.spinner_2);
+
+
+        final TextView rentVal_3      = (TextView) findViewById(R.id.rentVal_3);
+        final TextView transportVal_3 = (TextView) findViewById(R.id.transportVal_3);
+        final TextView foodVal_3      = (TextView) findViewById(R.id.foodVal_3);
+
+        final TextView total_3        = (TextView) findViewById(R.id.total_3);
+        final TextView balance2_3     = (TextView) findViewById(R.id.monthlyBalance2_3);
+        final TextView difference_3   = (TextView) findViewById(R.id.difference_3);
+        final Spinner s_3 = (Spinner) findViewById(R.id.spinner_3);
+
         // rentVal.setText(view.toString());
 
         balance2.setText(monthlyBalance1.getText().toString());
+        balance2_2.setText(monthlyBalance1_2.getText().toString());
+        balance2_3.setText(monthlyBalance1_3.getText().toString());
+
+
 
         s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -132,18 +190,66 @@ public class secondPage extends AppCompatActivity {
                 int totalInt   = Integer.parseInt(total   .getText().toString());
                 difference.setText(String.valueOf( balanceInt - totalInt ));
 
+                s_2.setSelection(position);
+                s_3.setSelection(position);
 
             }
-
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
+            }
+            });
 
+        s_2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedCity = parent.getSelectedItem().toString();
+                rentVal_2     .setText(String.valueOf(data.get( selectedCity)[0] ));
+                transportVal_2.setText(String.valueOf(data.get( selectedCity)[1] ));
+                foodVal_2     .setText(String.valueOf(data.get( selectedCity)[2] ));
+
+                total_2.setText(String.valueOf(data.get(selectedCity)[3]));
+                //change difference last, after copying balance1 to balance2
+
+                //difference = balance2 - total
+                int balanceInt = Integer.parseInt(balance2_2.getText().toString());
+                int totalInt   = Integer.parseInt(total_2   .getText().toString());
+                difference_2.setText(String.valueOf( balanceInt - totalInt ));
+
+                s.setSelection(position);
+                s_3.setSelection(position);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        s_3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedCity = parent.getSelectedItem().toString();
+                rentVal_3     .setText(String.valueOf(data.get( selectedCity)[0] ));
+                transportVal_3.setText(String.valueOf(data.get( selectedCity)[1] ));
+                foodVal_3     .setText(String.valueOf(data.get( selectedCity)[2] ));
+
+                total_3.setText(String.valueOf(data.get(selectedCity)[3]));
+                //change difference last, after copying balance1 to balance2
+
+                //difference = balance2 - total
+                int balanceInt = Integer.parseInt(balance2_3.getText().toString());
+                int totalInt   = Integer.parseInt(total_3   .getText().toString());
+                difference_3.setText(String.valueOf( balanceInt - totalInt ));
+
+                s.setSelection(position);
+                s_2.setSelection(position);
+
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
             }
         });
 
 
     }
-
-
+    
 
 }
