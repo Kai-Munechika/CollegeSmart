37c0b3de9638092a430bdb26edbf53f23744d5a3
Kai Munechika <kaim808@gmail.com>
2016-11-04 13:10:14 -0400

    Icon, text, and version update

--- app/src/main/java/edu/kaimbu/collegeapp0/qa.java
+++ app/src/main/java/edu/kaimbu/collegeapp0/qa.java
@@ -2,6 +2,8 @@ package edu.kaimbu.collegeapp0;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.widget.TextView;
 
 /**
  * Created by KaiM on 10/29/16.
@@ -13,6 +15,13 @@ public class qa extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.question_answer);
+
+        TextView rent = (TextView) findViewById(R.id.howRentIsCalculated);
+
+        String rentText = "Average rent is <b>not</b> the average rent of a one bedroom apartment in those locations. Rather, it is the calculated average rent that an individual pays to live in that city. Most recent college grads will split rent of an apartment among roommates and that number is the one listed as average rent.";
+
+        rent.setText(Html.fromHtml(rentText));
+
 
     }
 }
